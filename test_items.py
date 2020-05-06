c12a0b216f6d14fca27bcc9db4a086b4cae5b6c1
Victoria <solodovnikova.victoria@gmail.com>
2020-05-06 11:41:01 +0500

    grammar corrections

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_visibility(browser):
     browser.get(link)
-    #time.sleep(20) #for check --language=fr
-    button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    #time.sleep(20) #for checking --language=fr
+    button = browser.find_elements_by_css_selector("button.btn-add-to-basket")
     assert len(button) > 0, "button is not visible"
